d0dd64839bf0952508b7113d9736c4f28906ddfe
AffanIK <55394965+AffanIK@users.noreply.github.com>
2019-10-06 15:53:58 +0500

    Add files via upload

---
+++ CharacterRemover/CharacterRemover.vb
@@ -0,0 +1,30 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim str1, str2 As String
+        Dim myChar, nextChar As Char
+        Dim Counter As Integer
+
+        str1 = ""
+        str2 = ""
+        myChar = ""
+        nextChar = ""
+        Counter = 0
+
+        Console.Write("Enter String : ")
+        str1 = Console.ReadLine
+
+        Console.Write("Enter character to remove : ")
+        myChar = Console.ReadLine
+
+        For Counter = 1 To Len(str1)
+            nextChar = Mid(str1, Counter, 1)
+            If nextChar <> myChar Then
+                str2 = str2 & nextChar
+            End If
+        Next
+        Console.WriteLine("Final String = " & str2)
+        Console.ReadKey()
+    End Sub
+
+End Module
